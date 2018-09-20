b796a0936553eecb12bc2dbf7fc1a38de01275f0
Hairui <hairui@sorealprop.com>
2018-09-20 16:49:08 +0800

    First Commit

---
+++ Example/Pods/Target Support Files/Pods-SceneCoordinator_Example/Pods-SceneCoordinator_Example-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SceneCoordinator_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SceneCoordinator_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/SceneCoordinator/SceneCoordinator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SceneCoordinator : NSObject
+@end
+@implementation PodsDummy_SceneCoordinator
+@end
