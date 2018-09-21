616501224dc8dd7bab2e2a9f3d41bd9d244fd0c7
Hairui <hairui@sorealprop.com>
2018-09-21 15:43:14 +0800

    Modify actionSceneKey access control

--- SceneCoordinator/Classes/Core/SceneCoordinator.swift
+++ SceneCoordinator/Classes/Core/SceneCoordinator.swift
@@ -11,7 +11,7 @@ import UIKit
 public class SceneCoordinator<T : SceneType>{
     public typealias sceneType = T
     
-    public static var actionSceneKey : String{
+    private static var actionSceneKey : String{
         return "From:"
     }
     
